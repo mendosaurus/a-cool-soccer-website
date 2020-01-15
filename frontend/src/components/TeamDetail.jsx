381ebcc23a015a137fcf073ef20448480fa47d04
Masaki Endo <masaki.endo@nex.nikkei.com>
2020-01-15 18:30:18 +0900

    get team data from API in TeamDetail

--- frontend/src/components/TeamDetail.jsx
+++ frontend/src/components/TeamDetail.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class TeamDetail extends Component {
+  componentDidMount() {
+    console.log("component!", this.props);
+    let path = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${this.props.match.params.id}`;
+    axios.get(path).then(team => {
+      console.log(team);
+    });
+  }
+
   render() {
     return <div></div>;
   }
