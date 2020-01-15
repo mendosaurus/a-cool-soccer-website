0e878bf0404d4d506aeb3484278eac734066db90
Masaki Endo <masaki.endo@nex.nikkei.com>
2020-01-15 18:43:26 +0900

    display team name and badge in TeamDetail

--- frontend/src/components/TeamDetail.jsx
+++ frontend/src/components/TeamDetail.jsx
@@ -2,15 +2,39 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export default class TeamDetail extends Component {
+  state = {
+    theTeam: []
+  };
+
   componentDidMount() {
     console.log("component!", this.props);
     let path = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${this.props.match.params.id}`;
     axios.get(path).then(team => {
       console.log(team);
+      this.setState({
+        theTeam: team.data.teams[0]
+      });
+      console.log(this.state);
     });
   }
 
+  displayTeam = () => {
+    let team = this.state.theTeam;
+    return (
+      <div>
+        <div>{team.strTeam}</div>
+        <div>
+          <img src={team.strTeamBadge} alt="" />
+        </div>
+      </div>
+    );
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <div>{this.displayTeam()}</div>
+      </div>
+    );
   }
 }
