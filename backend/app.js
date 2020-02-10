504467fdb1d83e3b7a061823e60ab242a3b34839
Masaki Endo <masaki.endo@nex.nikkei.com>
2020-02-10 17:32:37 +0900

    configure the middleware to enable sessions in app.js

--- backend/app.js
+++ backend/app.js
@@ -30,6 +30,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: "basic-auth-secret",
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
 
 // Express View engine setup
 
